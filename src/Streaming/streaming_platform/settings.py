70f582f2edc927f9d3e1fd1b0647821669c1d983
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-05-30 11:41:27 -0300

    update

--- src/Streaming/streaming_platform/settings.py
+++ src/Streaming/streaming_platform/settings.py
@@ -42,7 +42,17 @@ INSTALLED_APPS = [
     'content_app',
     'drf_yasg',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+       'DEFAULT_AUTHENTICATION_CLASSES': [
+           'rest_framework.authentication.TokenAuthentication',
+       ],
+       'DEFAULT_PERMISSION_CLASSES': [
+           'rest_framework.permissions.IsAuthenticated',
+       ],
+   }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
